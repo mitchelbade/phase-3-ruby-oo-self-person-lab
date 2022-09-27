ce59373f8cbb6e053c551e812d3827433a86e16a
mitchelbade <mitchelm.bade@gmail.com>
2022-09-27 12:14:11 -0700

    commit tree 5ec96da52a7cafe8821a00e06119c5998e68248b

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,69 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = value.clamp(0, 10)
+    end
+
+    def hygiene=(value)
+        @hygiene = value.clamp(0, 10)
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene=(@hygiene += 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness=(@happiness += 2)
+        self.hygiene=(@hygiene -= 3)
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness=(@happiness += 3)
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            [self, friend].each { |person| person.happiness -= 2}
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            [self, friend].each { |person| person.happiness += 1}
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
